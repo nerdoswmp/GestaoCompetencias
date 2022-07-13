7fab97c9d6250c2e697e5ac8fa1a86b8ada769a2
Grinduim <viniciusgribelperson@gmail.com>
2022-07-13 09:46:48 -0300

    Aprendiz Register

--- Model/Aprendiz.cs
+++ Model/Aprendiz.cs
@@ -9,15 +9,24 @@ namespace GestaoCompetencias.Models
         {
             AprendizCompetencias = new HashSet<AprendizCompetencia>();
         }
-
         public int Id { get; set; }
         public string Nome { get; set; } = null!;
         public string Edv { get; set; } = null!;
         public int? LoginId { get; set; }
         public int? TurmaId { get; set; }
-
         public virtual Login? Login { get; set; }
         public virtual Turma? Turma { get; set; }
         public virtual ICollection<AprendizCompetencia> AprendizCompetencias { get; set; }
+
+        public int save(){
+            int id = 0;
+            using(var db = new DB_Gestao_CompetenciasContext()){
+                db.Aprendizes.Add(this);
+                db.SaveChanges();
+                id = this.Id;
+
+            }
+            return id;
+        }
     }
 }
--- Model/DB_Gestao_CompetenciasContext.cs
+++ Model/DB_Gestao_CompetenciasContext.cs
@@ -30,8 +30,7 @@ namespace GestaoCompetencias.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=JVLPC0587;Database=DB_Gestao_Competencias;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=" + Environment.MachineName + ";Database=DB_Gestao_Competencias;Trusted_Connection=True;");
             }
         }
 
